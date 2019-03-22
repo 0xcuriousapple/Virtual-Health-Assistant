10c570b73ccadeb854b29259941a0e006d1ea2ec
Abhishek Vispute <abhivispute33@gmail.com>
2019-03-22 07:29:41 +0530

    Removing jQuery dependency

--- js/main.js
+++ js/main.js
@@ -1,36 +1,41 @@
-$(document).ready(function(){
-	
-	$(".login1").click(function(){
+document.addEventListener("DOMContentLoaded",
+  function (event) {
 
-		$(".reg").removeClass("regopen");
-		$(".emergency").removeClass("emergencyopen");
-		$(".vendor").removeClass("vendoropen");
-		$(".login").toggleClass("loginopen");	
-	});	
+  var l = document.getElementsByClassName('login')[0];
+  var r = document.getElementsByClassName('reg')[0];
+  var v = document.getElementsByClassName('vendor')[0];
+  var e = document.getElementsByClassName('emergency')[0];
+   
+	function lf() {
+	 r.classList.remove("regopen");
+  	 e.classList.remove("emergencyopen");
+  	 v.classList.remove("vendoropen");
+	 l.classList.toggle("loginopen");
+	}
+    function rf() {
+ 	 l.classList.remove("loginopen");
+  	 e.classList.remove("emergencyopen");
+  	 v.classList.remove("vendoropen");
+	 r.classList.toggle("regopen");
+	}
+	function ef() {
+     l.classList.remove("loginopen");
+ 	 r.classList.remove("regopen");
+  	 v.classList.remove("vendoropen");
+	 e.classList.toggle("emergencyopen");
+	}
+	 function vf() {
+ 	 l.classList.remove("loginopen");
+ 	 r.classList.remove("regopen");
+  	 e.classList.remove("emergencyopen");
+	 v.classList.toggle("vendoropen");
+	}
 
-	$(".reg1").click(function(){
 
-		$(".login").removeClass("loginopen");
-		$(".emergency").removeClass("emergencyopen");
-		$(".vendor").removeClass("vendoropen");
+    login1.onclick =function(){lf()};
+    reg1.onclick =function(){rf()};
+    emergency1.onclick =function(){ef()};
+    vendor1.onclick =function(){vf()};
 
-		$(".reg").toggleClass("regopen");	
+    	
 	});
-
-	$(".emergency1").click(function(){
-		
-		$(".login").removeClass("loginopen");
-		$(".reg").removeClass("regopen");
-		$(".vendor").removeClass("vendoropen");
-
-		$(".emergency").toggleClass("emergencyopen");
-	});	
-
-	$(".vendor1").click(function(){
-		$(".login").removeClass("loginopen");
-		$(".reg").removeClass("regopen");
-		$(".emergency").removeClass("emergencyopen");
-
-		$(".vendor").toggleClass("vendoropen");
-	});	
-});
